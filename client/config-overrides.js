4dc7dc1fa24a2cce95e06556a4f2fc0c8fa4bd99
Kevin <kevin@kevinrockwell.dev>
2025-01-07 17:19:16 -0600

    Final Pre-Deploy Update2

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,56 +1,40 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
         buffer: require.resolve('buffer'),
-        process: require.resolve('process/browser.js'),
-        path: require.resolve('path-browserify'),
+        process: require.resolve('process/browser'),
         fs: false,
+        path: false,
         net: false,
         tls: false,
         child_process: false,
-        os: require.resolve('os-browserify/browser'),
-        zlib: require.resolve('browserify-zlib')
-    });
-    config.resolve.fallback = fallback;
+        os: false,
+        http: false,
+        https: false,
+        zlib: false
+    };
 
-    config.plugins = (config.plugins || []).concat([
+    config.plugins = [
+        ...config.plugins,
         new webpack.ProvidePlugin({
-            process: 'process/browser.js',
+            process: 'process/browser',
             Buffer: ['buffer', 'Buffer']
         }),
         new webpack.DefinePlugin({
             'process.env': JSON.stringify(process.env)
-        }),
-        new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
-            const mod = resource.request.replace(/^node:/, '');
-            switch (mod) {
-                case 'buffer':
-                    resource.request = 'buffer';
-                    break;
-                case 'stream':
-                    resource.request = 'readable-stream';
-                    break;
-                default:
-                    break;
-            }
-        }),
-        new webpack.NormalModuleReplacementPlugin(/^node-gyp-build$/, (resource) => {
-            resource.request = 'node-gyp-build/build.js';
         })
-    ]);
+    ];
 
-    // Ignore native addons
-    config.externals = {
-        ...config.externals,
-        'node-gyp-build': 'node-gyp-build'
-    };
-
-    config.resolve.extensions = [...(config.resolve.extensions || []), '.js', '.jsx'];
-    config.resolve.mainFields = ['browser', 'module', 'main'];
+    config.module.rules.push({
+        test: /\.m?js$/,
+        resolve: {
+            fullySpecified: false
+        }
+    });
 
     return config;
 }; 
