e860229cbc66cbd89bbeeb4ed0c0a47258ea5dd2
Kevin <kevin@kevinrockwell.dev>
2025-01-08 00:12:04 -0600

    build enhancements and refinements with Fixes

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,38 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['react-app', 'react-app/jest'],
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react'],
     rules: {
-        'prettier/prettier': 'off'
+        'react/prop-types': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-unused-vars': ['warn', {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^React$|^useState$|^useEffect$|^useContext$|^useRef$|^useNavigate$'
+        }],
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'react/jsx-no-undef': 'error'
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
     }
 };
--- client/src/utils/SignalStore.js
+++ client/src/utils/SignalStore.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 export class SignalProtocolStore {
   constructor() {
     this.store = {};
